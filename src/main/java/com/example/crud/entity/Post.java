b87c3a4918ad7aa4775a89a783e66079b755cc89
tiktokyi <tiktokyi@gmail.com>
2023-04-21 11:29:18 +0900

    Post entity 생성

---
+++ src/main/java/com/example/crud/entity/Post.java
@@ -0,0 +1,24 @@
+package com.example.crud.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name ="user_name", length = 30)
+    private String name;
+
+    @Column(name ="post_contents")
+    private String contents;
+
+    @ManyToOne                                                                                               //Member 엔티티와 ManyToOne의 관계를 가져 Member엔티이의 pk를 fk로 사용
+    private Member member;                                                                               //테이블생성시 자동적으로 member_id라는 필드명을 가진다.
+
+}
